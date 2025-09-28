abb257c16f386d1436e0bd6c75b88feca4fbcc37
ajay singh bisht <asblko84outlook.com>
2025-09-28 16:46:29 +0530

    test

---
+++ WebApplication1/Data/MyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+
+namespace WebApplication1.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions options) : base(options)
+        {
+
+
+        }
+
+      public  DbSet<Book> books { get; set; }
+    }
+}
+
